5f86be4a73bcfa2ab50b654be11b54ad7b311ced
Ben Shelbourne <ben.shelbourne@arm.com>
2024-12-09 08:11:21 +0000

    day 8 part 2: hacked it through in the end but chasing it!

--- 8/antenna-map.ts
+++ 8/antenna-map.ts
@@ -1,6 +1,7 @@
+import { group } from 'console';
 import * as fsPromise from 'fs/promises';
 
-class AntiNode {
+class Node {
     row: number;
     positionInRow: number;
 
@@ -10,19 +11,30 @@ class AntiNode {
     }
 
     isUnique(map: any[][]): boolean {
+        return true;
+    }
+}
+
+
+class AntiNode extends Node{
+    constructor(x: number, y: number) {
+        super(x, y);
+    }
+
+    isUnique(map: any[][]): boolean {
         return !(this.hasFrequencyAtPoint(map)) && this.inBounds(map);
     }
 
     inBounds(map: any[][]): boolean {
         return this.row > -1 &&
             this.positionInRow > -1 &&
-            this.row < map.length &&
-            this.positionInRow < map[0].length;
+            this.row < 50 &&
+            this.positionInRow < 50;
     }
 
     hasFrequencyAtPoint(map: any[][]): boolean {
         try {
-            if (map[this.row][this.positionInRow] instanceof AntiNode) {
+            if (map[this.row][this.positionInRow] instanceof Node) {
                 return true;
             }
         } catch(err) {
@@ -33,14 +45,11 @@ class AntiNode {
     }
 }
 
-class Frequency {
-    row: number;
-    positionInRow: number;
+class Frequency extends Node{
     frequency: string;
 
     constructor(x: number, y: number, freq: string) {
-        this.row = x;
-        this.positionInRow = y;
+        super(x, y);
         this.frequency = freq;
     }
 
@@ -54,26 +63,34 @@ class Frequency {
         let thisPos = 0;
         let cmoPos = 0;
         let distances = this.getDistance(cmp);
-        if (this.row < cmp.row) {
-            // above
-            thisRow = this.row - distances[0];
-            cmpRow = cmp.row + distances[0];
-        } else {
-            // below
-            thisRow = thisRow + distances[0];
-            cmpRow = cmp.row - distances[0];
-        }
 
-        if (this.positionInRow > cmp.positionInRow) {
-            // right
-            thisPos = this.positionInRow + distances[1];
-            cmoPos = cmp.positionInRow - distances[1];
-        } else {
-            // left
-            thisPos = this.positionInRow - distances[1];
-            cmoPos = cmp.positionInRow + distances[1];
+        let thisAntinodes = [];
+        let cmpAntinodes = [];
+
+        for (let i = 1; i < 50; i++) {
+            if (this.row < cmp.row) {
+                // above
+                thisRow = this.row - (distances[0] * i);
+                cmpRow = cmp.row + (distances[0] * i);
+            } else {
+                // below
+                thisRow = thisRow + (distances[0] * i);
+                cmpRow = cmp.row - (distances[0] * i);
+            }
+
+            if (this.positionInRow > cmp.positionInRow) {
+                // right
+                thisPos = this.positionInRow + (distances[1] * i);
+                cmoPos = cmp.positionInRow - (distances[1] * i);
+            } else {
+                // left
+                thisPos = this.positionInRow - (distances[1] * i);
+                cmoPos = cmp.positionInRow + (distances[1] * i);
+            }
+            thisAntinodes.push(new AntiNode(thisRow, thisPos));
+            cmpAntinodes.push(new AntiNode(cmpRow, cmoPos));
         }
-        return [new AntiNode(thisRow, thisPos), new AntiNode(cmpRow, cmoPos)];
+        return [...thisAntinodes, ...cmpAntinodes];
     }
 }
 
@@ -104,8 +121,11 @@ class FrequencyGroup {
 
 class FrequencyGroups {
     frequencyGroups: Array<FrequencyGroup> = [];
+    map: any[][];
 
-    constructor(){}
+    constructor(m: any[][]){
+        this.map = m;
+    }
 
     addFrequency(frequency: Frequency): Frequency {
         const freqIdx = this.frequencyGroups.map((group) => group.frequency).indexOf(frequency.frequency);
@@ -123,27 +143,36 @@ async function main() {
     const mapInput = await fsPromise.open('/home/benshe01/src/advent24/8/resources/input.txt', 'r');
     let map: any[][] = [];
     let lineNo = 0;
-    const frequencies = new FrequencyGroups();
+    const frequencies = new FrequencyGroups(map);
     for await (const line of mapInput.readLines()) {
         let row: any[] = [];
         line.split('').forEach((val, idx) => {
             const char = val.match(/([a-zA-Z]{1}|\d{1})/gm)?.[0];
             if (char) {
-                frequencies.addFrequency(new Frequency(lineNo, idx, char));
+                const freq = new Frequency(lineNo, idx, char);
+                frequencies.addFrequency(freq);
+                row.push(freq);
+            } else {
+                row.push(val);
             }
-            row.push(val);
         });
         map.push(row);
         lineNo++;
     }
 
     let uniqueAntinodes = [];
-    const antinodes: Array<AntiNode> = frequencies.frequencyGroups.map((group) => group.getAntinodes()).flat();
+    const antinodes: Array<Node> = frequencies.frequencyGroups.map((group) => group.getAntinodes()).flat();
 
     antinodes.forEach((antinode) => {
         if (antinode.isUnique(map)) {
             uniqueAntinodes.push(antinode);
         }
+    });
+
+    frequencies.frequencyGroups.forEach((group) => {
+        group.frequencies.forEach((freq) => {
+            uniqueAntinodes.push(freq);
+        });
     });
 
     console.log('Unique Antinodes:', uniqueAntinodes.length);
